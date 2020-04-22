2b4c206b010ae7a47d997ed1e7bfa60906151978
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-21 23:48:15 -0500

    const

--- 00_fundamentos/02_variablesLetYConst.js
+++ 00_fundamentos/02_variablesLetYConst.js
@@ -1,4 +1,8 @@
-let producto = 'Libro'; //let no permite redeclarar una variable solo reasignar su valor
-    producto = 'Lobro 1';
+// let producto = 'Libro'; //let no permite redeclarar una variable solo reasignar su valor
+//     producto = 'Lobro 1';
+
+const producto = 'Libro'; //las constantes siempre se deben inicializar
+        producto = 'Libro2'; //no se puede reasignar el valor a una constante
+        
 
 console.log(producto);
