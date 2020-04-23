40d2c74b6df393f4d20f25b9d94cc5afb18da71e
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-22 22:38:25 -0500

    numero y operaciones

---
+++ 00_fundamentos/04_numerosYOperaciones.js
@@ -0,0 +1,39 @@
+const numeroUno = 30;
+const numeroDos = 20;
+const numeroTres = 20.20;
+const numeroCuatro = .1020;
+const numeroCinco = -3;
+
+let resultado;
+//suma
+resultado = numeroUno + numeroDos;
+//resta
+resultado = numeroUno - numeroDos;
+//multiplicar
+resultado = numeroUno * numeroDos;
+resultado = numeroUno * 2;
+//division
+resultado = numeroUno / numeroDos;
+//modulo
+resultado = numeroUno % numeroDos;
+//Pi
+resultado = Math.PI;
+//redondear
+resultado = Math.round(2.3)
+resultado = Math.ceil(2.3) //redondea hacia arriba
+resultado = Math.floor(2.3) //redondea hacia abajo
+//raiz cuadrada
+resultado = Math.sqrt(144);
+//absoluto
+resultado = Math.abs(numeroCinco);
+//potencia
+resultado = Math.pow(8,3);
+//minimo
+resultado = Math.min(1,2,3,5,4,6,7,8,9);
+//maximo
+resultado = Math.max(1,2,3,5,4,6,7,8,9);
+//aleatorio
+resultado = Math.random();
+
+
+console.log(resultado);
