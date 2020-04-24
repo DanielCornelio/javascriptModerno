c6d93636cccecfd852f4fe5a398ba4903676fe2c
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-23 22:15:55 -0500

    tipos de datos

---
+++ 00_fundamentos/05_tiposDeDatos.js
@@ -0,0 +1,17 @@
+//datos primitivos
+let dato = "daniel";
+    dato = 20;
+    dato = 20.20;
+    dato = -30;
+    dato = "20";
+    dato = true; //booleanos
+    dato = null; //null
+    dato = undefined;
+    dato = Symbol('simbolo'); //symbol
+//datos de referencia
+    dato = [1,2,3,4];//arreglo
+    dato = {nombre:"daniel", profesion: "developer"};//objeto
+    dato = new Date();//fecha
+
+
+console.log(typeof dato);
