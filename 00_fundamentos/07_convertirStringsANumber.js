91f7878c2642ddaae88a7a538800fd7306da4e45
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-23 23:04:22 -0500

    convertir cadenas de numeros a numeros

--- 00_fundamentos/07_convertirStringsANumber.js
+++ 00_fundamentos/07_convertirStringsANumber.js
@@ -8,3 +8,24 @@ console.log(parseInt(numero1) + numero2);
 
 console.log(typeof parseInt(numero1));
 console.log(typeof parseInt(numero3)); //realmente es NaN
+
+let dato;
+dato = Number("20");
+dato = Number("20.22033");
+dato = Number(true);
+dato = Number(false);
+dato = Number(null);
+dato = Number(undefined);
+dato = Number("Hola");
+dato = Number([1,2,3,4]);
+
+dato = parseInt("100");
+dato = parseFloat("100");
+dato = parseFloat("100.2030");
+dato = parseInt("100.2030");
+
+//toFixed
+
+dato = 155151.2648878;
+
+console.log(parseFloat(dato).toFixed(2));
