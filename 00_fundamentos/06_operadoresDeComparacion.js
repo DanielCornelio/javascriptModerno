0edb3790b36a520d6743097421a174a2793eb463
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-23 22:33:32 -0500

    operadores de comparacion

---
+++ 00_fundamentos/06_operadoresDeComparacion.js
@@ -0,0 +1,18 @@
+const numero1 = 20;
+const numero2 = 30;
+const numero3 = "20";
+
+console.log(numero1 > numero2);
+console.log(numero1 < numero2);
+
+//comparador igual
+console.log(numero1 === numero3);
+
+//diferente
+console.log(2 != 3);
+
+//comparacion de cadenas
+console.log("hola" == " hola");
+console.log('z'>'A');
+
+console.log(null === undefined);
