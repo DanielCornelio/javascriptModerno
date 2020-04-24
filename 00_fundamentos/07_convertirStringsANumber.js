98544d39a861d361dd5e5cdec6a8a22611a19fb4
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-23 22:53:13 -0500

    convertir cadenas de numeros a numeros

---
+++ 00_fundamentos/07_convertirStringsANumber.js
@@ -0,0 +1,10 @@
+const numero1 = "50";
+const numero2 = 10;
+const numero3 = "tres";
+
+console.log(numero1 + numero2);
+console.log(Number(numero1) + numero2);
+console.log(parseInt(numero1) + numero2);
+
+console.log(typeof parseInt(numero1));
+console.log(typeof parseInt(numero3)); //realmente es NaN
