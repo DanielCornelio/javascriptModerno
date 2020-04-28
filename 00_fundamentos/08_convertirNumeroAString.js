1131edf06c1a1cde3f72471685d29fdd662d06fa
Daniel Cornelio <jdcornelio89@gmail.com>
2020-04-27 23:49:55 -0500

    convertir numero a string

---
+++ 00_fundamentos/08_convertirNumeroAString.js
@@ -0,0 +1,19 @@
+let cp;
+cp = 8600;
+
+cp = String(cp);
+
+console.log(cp);
+console.log(cp.length);
+
+//booleano
+let dato;
+dato = true;
+dato = String(dato)
+
+//array a string
+dato = String([1,2,3,4])
+//toString();
+dato = 20;
+dato = dato.toString();
+console.log(dato)
