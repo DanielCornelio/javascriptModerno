48f739a9c08d3daff63e695aedfc6e30177449ae
Daniel Cornelio <jdcornelio89@gmail.com>
2020-05-03 21:24:45 -0500

    arreglos con objetos

---
+++ 00_fundamentos/13_arregloConObjetos.js
@@ -0,0 +1,10 @@
+const autos = [
+    {modelo : "mustang", motor : 6.2},
+    {modelo : "camaro", motor : 6.1},
+    {modelo : "challenger", motor : 6.3},
+
+];
+
+for(let i = 0; i<autos.length; i++){
+    console.log(`${autos[i].modelo} ${autos[i].motor}`);
+}
