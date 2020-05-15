664f55d496c2c4c6f9b298ae53731ccb0e337825
Daniel Cornelio <jdcornelio89@gmail.com>
2020-05-15 17:16:44 -0500

    seleccionando un elemento y aplicandole propiedades

---
+++ 01_DOM-Window-Scripting/original/universidad/js/02_seleccionandoUnElemento.js
@@ -0,0 +1,11 @@
+//getElementById
+
+let encabezado;
+    encabezado = document.getElementById('encabezado');
+    encabezado.style.background="#333";
+    encabezado.style.color="#fff";
+    encabezado.style.padding="20px";
+    //cambiar texto
+    encabezado.textContent="Los mejores cursos"
+
+    console.log(encabezado)
