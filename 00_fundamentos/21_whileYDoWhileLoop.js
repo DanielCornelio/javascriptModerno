15e8d0ec4936c4e6a6ab6f7e05d53995efdd710e
Daniel Cornelio <jdcornelio89@gmail.com>
2020-05-15 09:56:43 -0500

    while y do while loop

---
+++ 00_fundamentos/21_whileYDoWhileLoop.js
@@ -0,0 +1,16 @@
+const musica = ["cancion 1", "cancion 2", "cancion 3"];
+
+let i = 0;
+
+while(i < musica.length){
+    console.log(`Reprocuciento cancion: ${musica[i]}`);
+    i++;
+}
+
+
+let j = 1000;
+
+do{
+    console.log(`Numero: ${j}`);
+    j++;
+}while(j < 10);
