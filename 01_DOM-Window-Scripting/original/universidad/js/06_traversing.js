31982e2fe2649c5a08fc79f7de70f24c1f9f6cea
Daniel Cornelio <jdcornelio89@gmail.com>
2020-06-16 21:56:44 -0500

    traversing

---
+++ 01_DOM-Window-Scripting/original/universidad/js/06_traversing.js
@@ -0,0 +1,10 @@
+const cursos = document.querySelectorAll('.card');
+
+console.log(cursos[0].childElementCount);
+
+//1 = elementos html
+//2 = atributos
+//3 = texto
+//8 = comentarios
+//9 = documentos
+//10 = doctype
